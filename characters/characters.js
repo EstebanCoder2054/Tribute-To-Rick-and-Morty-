a82a938310bed22e67ac0b52e6e9ec1d89d7f756
Esteban <esteban-escobar1111@hotmail.com>
2019-07-08 18:55:38 -0500

    elementos creados dinámicamente pero sin estilos

--- characters/characters.js
+++ characters/characters.js
@@ -1,10 +1,7 @@
 let arrayImagenes = [];
 let arrayNombres = [];
 let arrayStatus = [];
-let placeHolderImg = document.querySelectorAll('.thumbnail-container img');
-let placeHolderNombre = document.querySelectorAll('.thumbnail-container h5');
-let placeHolderStatus = document.querySelectorAll('.thumbnail-container p');
-    
+  
 fetch('https://rickandmortyapi.com/api/character/?page=1')
     .then((response) => response.json())
     .then( function(data){
@@ -15,14 +12,46 @@ fetch('https://rickandmortyapi.com/api/character/?page=1')
             arrayNombres[i] = data.results[i].name;
             arrayStatus[i] = data.results[i].status;
             
-            placeHolderImg[i].src = arrayImagenes[i];
-            placeHolderNombre[i].innerText = arrayNombres[i];
-            placeHolderStatus[i].innerText = arrayStatus[i];
-
+         
+            let nuevoDiv = document.createElement('div');
+            let divAdentro = document.createElement('div');
+            nuevoDiv.appendChild(divAdentro);
+        
+            let divRecargado = `
+                
+            <div class="container">   
+            
+            
+            <div class="card-columns thumbnail-container">
+                
+            
+            <div class="card" style="background-color: #C8D5E8">
+            <img src=${arrayImagenes[i]} class="card-img-top img-fluid" alt="..." >
+            <div class="card-body">
+                <h5 class="card-title">${arrayNombres[i]}</h5>
+                <p class="card-text">${arrayStatus[i]}</p>
+            </div>
+            </div>
+            
+            
+            </div> 
+            
+            
+            </div>
+            
+            `;
+            nuevoDiv.innerHTML = divRecargado;
+            document.body.appendChild(nuevoDiv);
+            
+    
+            console.log("prueba");
+            console.log(divRecargado);
+            
+            
         }
 
         //llamando a la función cambiarImagenesDinamicas y le envío el arreglo con las imagenes, títulos y status
-        cambiarImagenesDinamicas( placeHolderImg, placeHolderNombre, placeHolderStatus )
+        //cambiarImagenesDinamicas( placeHolderImg, placeHolderNombre, placeHolderStatus )
 
         return data;
     } )
@@ -30,63 +59,47 @@ fetch('https://rickandmortyapi.com/api/character/?page=1')
 
 
 
-function cambiarImagenesDinamicas( placeHolderImg, placeHolderNombre, placeHolderStatus ){
+// function cambiarImagenesDinamicas( placeHolderImg, placeHolderNombre, placeHolderStatus ){
             
-    for(let i = 0; i < 7; i++){
+    
 
-        let nuevoDiv = document.createElement('div');
-        let divAdentro = document.createElement('div');
-        nuevoDiv.appendChild(divAdentro);
-        document.body.appendChild(nuevoDiv);
+//         let nuevoDiv = document.createElement('div');
+//         let divAdentro = document.createElement('div');
+//         nuevoDiv.appendChild(divAdentro);
+//         document.body.appendChild(nuevoDiv);
         
-        let divRecargado = `
+//         let divRecargado = `
             
-        <div class="container">   
+//         <div class="container">   
         
         
-        <div class="card-columns thumbnail-container">
+//         <div class="card-columns thumbnail-container">
             
         
-        <div class="card" style="background-color: #C8D5E8">
-        <img src="${placeHolderImg[i]}" class="card-img-top img-fluid" alt="..." >
-        <div class="card-body">
-            <h5 class="card-title">${placeHolderNombre[i]}</h5>
-            <p class="card-text">${placeHolderStatus[i]}</p>
-        </div>
-        </div>
-        
-        <div class="card" style="background-color: #C8D5E8">
-        <img src="${placeHolderImg[i+1]}" class="card-img-top img-fluid" alt="..." >
-        <div class="card-body">
-            <h5 class="card-title">${placeHolderNombre[i+1]}</h5>
-            <p class="card-text">${placeHolderStatus[i+1]}</p>
-        </div>
-        </div>
-        
-        <div class="card" style="background-color: #C8D5E8">
-        <img src="${placeHolderImg[i+2]}" class="card-img-top img-fluid" alt="..." >
-        <div class="card-body">
-            <h5 class="card-title">${placeHolderNombre[i+2]}</h5>
-            <p class="card-text">${placeHolderStatus[i+2]}</p>
-        </div>
-        </div>
-        
-        </div> 
+//         <div class="card" style="background-color: #C8D5E8">
+//         <img src="${placeHolderImg[i]}" class="card-img-top img-fluid" alt="..." >
+//         <div class="card-body">
+//             <h5 class="card-title">${placeHolderNombre[i]}</h5>
+//             <p class="card-text">${placeHolderStatus[i]}</p>
+//         </div>
+//         </div>
         
         
+//         </div> 
         
-        </div>
         
-        `;
-        nuevoDiv.innerHTML = divRecargado;
+//         </div>
         
-        i = i+1;
+//         `;
+//         nuevoDiv.innerHTML = divRecargado;
+        
+        
 
-        console.log("prueba");
+//         console.log("prueba");
 
-        }
+        
         
         
     
-    }
+//     }
     
